454359db33825c597f0916c979b2d80a8c87fa07
sami akhtar <samiakhter47@gmail.com>
2024-12-21 00:49:19 +0530

    "changes in code"

--- src/index.js
+++ src/index.js
@@ -1,17 +1,23 @@
-import dotenv from "dotenv"
+import dotenv from "dotenv";
 import connectDB from "./db/index.js";
+import express from 'express'
 
 dotenv.config({
-    path:'./env'
-})
+    path: './env'
+});
 
-connectDB()
-.then(()=>{
-    app.listen(process.env.PORT || 8000, ()=>{
-        console.log(`Server is running at port: 
-            ${process.env.PORT}`);
-    })
-})
-.catch(()=>{
-    console.log("MONGO db connection failed !!!",err);
-})
+const app = express();
+
+const startServer = async () => {
+    try {
+        await connectDB();
+        app.listen(process.env.PORT || 8000, () => {
+            console.log(`Server is running at port: ${process.env.PORT}`);
+        });
+    } catch (error) {
+        console.log("MONGO db connection failed !!!", error);
+        process.exit(1);
+    }
+};
+
+startServer();
--- src/utils/apiError.js
+++ src/utils/apiError.js
@@ -4,7 +4,7 @@ class ApiError extends Error {
              statusCode,
              message="Something Went Wrong",
              errors = [],
-             statck = ""
+             stack = ""
     ){
         super(message)
         this.statusCode = statusCode
@@ -14,8 +14,8 @@ class ApiError extends Error {
         this.error = errors
 
 
-        if (statck) {
-            this.stack = statck
+        if (stack) {
+            this.stack = stack
         }else{
             Error.captureStackTrace(this.this.constructor)
         }
